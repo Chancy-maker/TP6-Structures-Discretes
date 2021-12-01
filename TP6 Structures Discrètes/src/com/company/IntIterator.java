ac9d5eb3ba7aa7b1921aeb1984a57c9aa54b46a5
Pierre <pierreakpata8@gmail.com>
2021-12-01 13:17:04 +0100

    Documentation des dernières méthode non documentés.

--- TP6 Structures Discrètes/src/com/company/IntIterator.java
+++ TP6 Structures Discrètes/src/com/company/IntIterator.java
@@ -1,6 +1,19 @@
 package com.company;
 
+/**
+ * Interface IntIterator.
+ */
 public interface IntIterator {
+    /**
+     * La méthode next() retourne la donnée portée par cette position
+     * courante et passe à la position suivante (qui devient la nouvelle position courante.
+     * @return
+     */
     int next();
+
+    /**
+     *  la méthode hasNext() permet de testé  si la position courante possède une position suivante
+     * @return
+     */
     boolean hasNext();
 }
--- TP6 Structures Discrètes/src/com/company/IntListIterator.java
+++ TP6 Structures Discrètes/src/com/company/IntListIterator.java
@@ -1,10 +1,22 @@
 package com.company;
 
 public class IntListIterator implements IntIterator{
+
+    /**
+     * List à parcourir
+     */
     private Cell list;
+
+    /**
+     * Maillon courant
+     */
     private Cell currentCell;
 
 
+    /**
+     * Constructeur de la classe IntListIterator
+     * @param list
+     */
     public IntListIterator(Cell list){
         this.list = list;
         this.currentCell = list;
